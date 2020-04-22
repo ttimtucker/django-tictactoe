d265f61ca7a02f50032da57b490c22de4b46a1c6
Tim Tucker <tim@ttimtucker.com>
2020-04-22 09:19:52 -0400

    “comment”

--- tictac/views.py
+++ tictac/views.py
@@ -33,9 +33,10 @@ class TicTacToe():
                 return True
 
         # check for diagonal winners
+        print('diag1=: ', )
         if [self.grid[i][i] for i in range(3)] == winner[player]: # This list comp transposes diag1 to row
                 return True
-        if [self.grid[i][i] for i in range(2,-1,-1)] == winner[player]: # This list comp transposes diag2 to row
+        if [self.grid[i][2-i] for i in range(3)] == winner[player]: # This list comp transposes diag2 to row
                 return True
 
         return False
@@ -73,7 +74,7 @@ class TicTacToe():
 
 def index(request):
     context={}
-    print("in index")
+    #print("in index")
     return render(request, 'tictac/index.html', context)
     #return HttpResponse("Hello, world.")
 
@@ -81,7 +82,7 @@ def update_grid(grid,position,player):
     # position is 1-9
     row=int((position-1)/3)
     col=(position-1)%3
-    print("in update_grid: row,col= ",row, col)
+    #print("in update_grid: row,col= ",row, col)
     grid[row][col]=player
     return grid
 
@@ -92,10 +93,11 @@ def is_move_valid(grid,position,player):
     if existing_entry == EMPTY:
         grid[row][col] = player
         return True
-    if existing_entry == player:
-        print("\n*****Error: you already moved to this location.  Try again.\n")
+    """if existing_entry == player:
+        #print("\n*****Error: you already moved to this location.  Try again.\n")
     else:
-        print("\n*****Error: your opponent already claimed this move.  Try again.\n")
+        #print("\n*****Error: your opponent already claimed this move.  Try again.\n")
+        """
     return False
 
 def is_winner(grid,player):
@@ -114,19 +116,22 @@ def is_winner(grid,player):
     # check for diagonal winners
     if [grid[i][i] for i in range(3)] == winner[player]: # This list comp transposes diag1 to row
             return True
-    if [grid[i][i] for i in range(2,-1,-1)] == winner[player]: # This list comp transposes diag2 to row
+    if [grid[i][2-i] for i in range(3)] == winner[player]: # This list comp transposes diag2 to row
             return True
 
     return False
 
+def grid_full(grid):
+    return not EMPTY in grid[0]+grid[1]+grid[2]
+
 def gridclick(request):
-    context={}
-    print (request.method)
+
+    #print (request.method)
     #button_form=ButtonForm(request.GET)
 
     if request.method == 'GET':
-        print("in GET section")
-        print (request.GET)
+        #print("in GET section")
+        #print (request.GET)
         if 'b0' in request.GET:
             clicked = 1
         elif 'b1' in request.GET:
@@ -151,23 +156,32 @@ def gridclick(request):
             request.session['grid']=NEW_GRID
             request.session['player']='X'
             grid=NEW_GRID
-        if clicked:
-            print("button {} pressed".format(clicked))
-            print("grid before update=",request.session['grid'])
+            request.session['game_over']=False
+            request.session['status_message'] = ''
+        if clicked and not request.session['game_over']:
+            #print("button {} pressed".format(clicked))
+            #print("grid before update=",request.session['grid'])
             request.session['clicked']=clicked
             player=request.session['player']
 
             # See if move is valid
-            if is_move_valid(request.session['grid'],clicked, player):
+            if is_move_valid(request.session['grid'],clicked, player) and not request.session['game_over']:
                 # modify our grid to reflect button clicked
                 new_grid=update_grid(request.session['grid'],clicked, player)
-                print(new_grid)
+                #print(new_grid)
                 request.session['grid']=new_grid
                 grid=new_grid
 
                 # Check if any player has won
                 if is_winner(grid, player):
-                    print("Player {} has won!".format(player))
+                    #print("Player {} has won!".format(player))
+                    request.session['status_message']='Player {} has won!'.format(player)
+                    request.session['game_over']=True
+                elif grid_full(grid):
+                    #print("Grid is full, but no winner")
+                    request.session['status_message'] = 'Grid is full, but no winner'
+                    request.session['game_over'] = True
+
                 else:
                     # Toggle player
                     if request.session['player'] == 'X':
@@ -176,11 +190,12 @@ def gridclick(request):
                     else:
                         request.session['player'] = 'X'
                         player = 'X'
+                    request.session['status_message'] = 'Player {}, please make your move.'.format(player)
             else:
                 # An invalid move was made, so no change to grid
                 grid=request.session['grid']
 
             # prepare context so that new grid is rendered
-        context={'grid': grid}
-        print("context=: ", context)
+        context={'grid': request.session['grid'], 'message': request.session['status_message']}
+        #print("context=: ", context)
     return render(request, 'tictac/tictactoe.html', context)
